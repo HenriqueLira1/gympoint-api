f43c981a411e0c85f0285f2a03dd613fcc1db8a4
Henrique <henriquelira99@gmail.com>
2019-11-11 22:37:21 -0300

    Add plans classes

---
+++ src/app/controllers/PlanController.js
@@ -0,0 +1,68 @@
+import * as Yup from 'yup';
+import Plan from '../models/Plan';
+
+class PlanController {
+    async index(req, res) {
+        const plans = await Plan.findAll({
+            attributes: ['id', 'title', 'duration', 'price'],
+        });
+
+        return res.json(plans);
+    }
+
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            title: Yup.string().required(),
+            duration: Yup.number().required(),
+            price: Yup.number().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
+        const { title, duration, price } = await Plan.create(req.body);
+
+        return res.json({ title, duration, price });
+    }
+
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            title: Yup.string(),
+            duration: Yup.number(),
+            price: Yup.number(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
+        const { id } = req.params;
+
+        const plan = await Plan.findByPk(id);
+
+        if (!plan) {
+            return res.status(400).json({ error: 'Plan not found' });
+        }
+
+        const { title, duration, price } = await plan.update(req.body);
+
+        return res.json({ id, title, duration, price });
+    }
+
+    async delete(req, res) {
+        const { id } = req.params;
+
+        const plan = await Plan.findByPk(id);
+
+        if (!plan) {
+            return res.status(400).json({ error: 'Plan not found' });
+        }
+
+        await plan.destroy();
+
+        return res.json({ id });
+    }
+}
+
+export default new PlanController();
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import authMiddleware from './app/middlewares/auth';
 
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 const routes = new Router();
 
@@ -10,5 +11,10 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.post('/students', StudentController.store);
+
+routes.get('/plans', PlanController.index);
+routes.post('/plans', PlanController.store);
+routes.put('/plans/:id', PlanController.update);
+routes.delete('/plans/:id', PlanController.delete);
 
 export default routes;
